49adf15878c4ba141b555b82d50ab6714950d6d3
Lewis Jones <lewisgwynjones@gmail.com>
2023-06-25 20:26:19 +0000

    add 0 at bottom of each split for vote counter

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const container = document.querySelector('.container')
+const btn = document.querySelectorAll('.btn')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+
+
+btn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.innerText = 'Thanks for Voting!';
+    });
+});
